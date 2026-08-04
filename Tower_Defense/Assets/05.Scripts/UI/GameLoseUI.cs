e78ec4b193a069006a15d4ea91246f0515666d92
d00sa <tkfrn0304@naver.com>
2026-08-04 18:12:18 +0900

    26.08.04
    Bugs + Animation

--- Tower_Defense/Assets/05.Scripts/UI/GameLoseUI.cs
+++ Tower_Defense/Assets/05.Scripts/UI/GameLoseUI.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class GameLoseUI : MonoBehaviour
 {
+    [SerializeField] RectTransform _panels;
     public void Start()
     {
         SoundManager.Instance.StopAllSounds();
+
+        _panels.localScale = Vector3.one;
         SoundManager.Instance.PlaySFX("GameLose");
     }
     public void ReStart()
     {
         SoundManager.Instance.PlaySFX("ButtonClick");
+
+        _panels.localScale = Vector3.zero;
         GameManager.Instance.GoToDifficultySelect();
     }
 
